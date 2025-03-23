d37d95ad1e52c1dace76c7c077dea0309596e177
Vishal-kajaria <120918053+Vishal-kajaria@users.noreply.github.com>
2025-03-23 18:00:57 +0530

    cloning data of an array of objects in javascript
    This repository serves as a structured learning platform for HTML, CSS, and Responsive Web Design (RWD)
    
    Key Features:
    Comprehensive HTML Tutorials: Covers foundational to advanced HTML concepts, including semantic elements and best practices.
    
    CSS Styling & Layouts: Explores modern styling techniques, including Flexbox, Grid, animations, and design principles.
    
    Responsive Web Design (RWD): Guides users in creating fully responsive web pages using media queries and adaptive design strategies.
    
    Intuitive Navigation: Structured topic-wise linking for smooth redirection to relevant tutorial pages.
    
    Beginner to Advanced Learning Path: Suitable for individuals at various levels of expertise.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // section mobile - btn 
 
+import { Tutorials } from "./Data/tutorial.js"
+
 let MenuBtnMobileEl = document.getElementById("menu-btn-mobile")
 let MenuBtnMobileElOpenBoolean = false
 let mobileMenuNavEl = document.getElementById("mobile-menu-nav")
@@ -24,10 +26,22 @@ mobileNavCloseEl.addEventListener("click", () => {
     mobileMenuNavEl.classList.remove("mobile-menu-nav-hidden")
     MenuBtnMobileElOpenBoolean = false
 })
+
+// ****************Tutorial-Btn ***********************
+
 const NestedNavigationContainer=document.getElementById("nested-navigation-container_id")
 let TutorialEl = document.getElementById("menu-btn-tutorial")
-TutorialEl.addEventListener("click",()=>{
-    TutorialEl.classList.toggle(".bg-green")
-    TutorialEl.classList.toggle(".text-white")
+const nestedNavigationCloseBtn = document.getElementById("nested-navigation-close-Btn")
+
+TutorialEl.addEventListener("click",toggleTutorial)
+nestedNavigationCloseBtn.addEventListener("click",toggleTutorial)
+
+function toggleTutorial(){
+    TutorialEl.classList.toggle("bg-green")
+    TutorialEl.classList.toggle("text-white")
     NestedNavigationContainer.classList.toggle("nested_navigation_hidden")
-})
+} 
+
+
+// **************Tutorial data**************** 
+console.log(Tutorials)
