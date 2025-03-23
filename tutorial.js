50b4a136053b674672aa322dfdd0842eebdb28c3
Vishal-kajaria <120918053+Vishal-kajaria@users.noreply.github.com>
2025-03-23 18:01:54 +0530

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,20 @@
+export const Tutorials= {
+    name : "Tutorials",
+    HTMLAndCSS :{
+        name : "HTML and CSS",
+        menu : [
+            {
+                title : "Learn HTML",
+                link : "https://www.geeksforgeeks.org/html-tutorial/"
+            },
+            {
+                title : "Learn CSS",
+                link : "https://www.geeksforgeeks.org/css-tutorial/"
+            },
+            {
+                title : "Learn RWD",
+                link : "https://www.geeksforgeeks.org/html-responsive-web-design/"
+            }
+        ]
+    }
+}
